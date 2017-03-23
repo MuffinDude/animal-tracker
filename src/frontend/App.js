e921a66779e848346ae0ba9dbaba4657375a6e55
Marten Truu <marten.truu@gmail.com>
2017-03-23 17:14:56 +0200

    🏛add bootsrap css as well

--- src/frontend/App.js
+++ src/frontend/App.js
@@ -1,19 +1,19 @@
 import React from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 import CreateAnimalForm from './createAnimalForm'
-
 import './App.css'
+
 
 const App = () => (
   <Router>
     <div className="container-fluid">
       <div className="row">
         <div className="col-12 col-md-8 offset-md-2 col-xl-6 offset-md-3">
-
+          <Route path="/new" component={CreateAnimalForm} />
         </div>
       </div>
-      <Route path="/new" component={CreateAnimalForm} />
     </div>
   </Router>
 )
