6be174d86d5259bb708d56dbd346408e431359b5
Marten Truu <marten.truu@gmail.com>
2017-03-23 17:11:31 +0200

    🏙add bootstrap column layout

--- src/frontend/App.js
+++ src/frontend/App.js
@@ -7,11 +7,15 @@ import './App.css'
 
 const App = () => (
   <Router>
-    <div className="App">
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col-12 col-md-8 offset-md-2 col-xl-6 offset-md-3">
+
+        </div>
+      </div>
       <Route path="/new" component={CreateAnimalForm} />
     </div>
   </Router>
 )
-
 
 export default App
