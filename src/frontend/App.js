81a969539294ba81a8a38b147dd6694f75b6780f
Marten Truu <marten.truu@gmail.com>
2017-03-30 13:32:47 +0300

    🐳 add docker db, some frontend changes

--- src/frontend/App.js
+++ src/frontend/App.js
@@ -1,19 +1,29 @@
 import React from 'react'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 import CreateAnimalForm from './createAnimalForm'
 import SearchAnimalForm from './searchAnimalForm'
 import './App.css'
 
-
 const App = () => (
   <Router>
-    <div className="container-fluid">
+    <div className="container-fluid py-5">
       <div className="row">
         <div className="col-12 col-md-8 offset-md-2 col-xl-6 offset-md-3">
-          <Route path="/new" component={CreateAnimalForm} />
-          <Route path="/search" component={SearchAnimalForm} />
+          <div className="d-flex justify-content-space-between">
+            <h1>Animal Farm</h1>
+            <div className="ml-auto">
+              <Link className="mr-3" to="/">Home</Link>
+              <Link className="" to="/search">Search</Link>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-12 col-md-8 offset-md-2 col-xl-6 offset-md-3">
+          <Route exact path="/" component={CreateAnimalForm} />
+          <Route path="/search" component={() => (<SearchAnimalForm />)} />
         </div>
       </div>
     </div>
--- src/frontend/searchAnimalForm/SearchAnimalForm.js
+++ src/frontend/searchAnimalForm/SearchAnimalForm.js
@@ -1,7 +1,38 @@
-import React from 'react'
+import React, { Component, PropTypes } from 'react'
 
-const SearchAnimalForm = () => (
-  <div>search for animals</div>
-)
+class SearchAnimalForm extends Component {
+  constructor(props) {
+    super(props)
+    this.onInputChange = this.onInputChange.bind(this)
+    this.state = {
+      inputValue: '',
+    }
+  }
+
+  onInputChange(event) {
+    this.setState({ inputValue: event.target.value })
+  }
+
+  render() {
+    const { inputValue } = this.state
+    return (
+      <div>
+        {this.props.myStringProp}
+        {inputValue}
+        <input
+          onChange={this.onInputChange}
+        />
+      </div>
+    )
+  }
+}
+
+SearchAnimalForm.propTypes = {
+  myStringProp: PropTypes.string,
+}
+
+SearchAnimalForm.defaultProps = {
+  myStringProp: 'noku',
+}
 
 export default SearchAnimalForm
